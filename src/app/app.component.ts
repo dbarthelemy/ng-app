b83b39ba5cead4e0538619438319b62096bd41db
dbarthelemy <david.barthelemy@novamap.fr>
2019-10-03 15:07:53 +0200

    Syntaxe de base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-app';
+  object = {
+    title: 'My app',
+    author: 'DBAR'
+  }
+  btnDisabled = true;
+  date = new Date();
+  products = [
+    { name: 'Product1', price: '111' },
+    { name: 'Product2', price: '222' }
+  ];
+
+  isBtnDisabled() {
+    return (this.btnDisabled);
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  getClasses(): string[] {
+    const classes: string[] = new Array();
+    classes.push('classCommon');
+    if (this.isBtnDisabled()) {
+      classes.push('class1');
+    } else {
+      classes.push('class2');
+    }
+    return (classes);
+  }
 }
