6cd9b706ed18201ba281915eab0b33ed4cd56d18
dbarthelemy <david.barthelemy@novamap.fr>
2019-10-04 12:24:00 +0200

    Observable & Operators

--- src/app/modules/product/services/product.service.ts
+++ src/app/modules/product/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs/internal/observable/of';
+import { map, filter, reduce } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -29,5 +30,15 @@ export class ProductService {
     // Observer got a next value: 2
     // Observer got a next value: 3
     // Observer got a complete notification
+
+    myObservable
+      .pipe(
+        map(x => x * 2),
+        filter(x => x > 3),
+        reduce((accumulator, x) => accumulator + x, 5)
+      )
+      .subscribe(
+        (x) => { console.log('Partial observer got a next value: ' + x); }
+      );
   }
 }
